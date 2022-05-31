8a2a6289f2bf11f904ff2ee95067c0903310f14a
Kai_EV <kasicer1@gmail.com>
2022-05-31 20:52:41 +0200

    Add Config.properties

--- src/db/AccesoDB.java
+++ src/db/AccesoDB.java
@@ -1,8 +1,13 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class AccesoDB {
 	
@@ -12,9 +17,26 @@ public class AccesoDB {
 	
 	
 	public AccesoDB() {
+		Properties prop = new Properties(); // Creamos un objeto de tipo properties para poder manejar el configDB
+		InputStream is = null; // Usamos un atributo del tipo String el cual sera el canal de comunicacion con la DB
 		
-		driver = "org.sqlite.JDBC";
-		url = "jdbc:sqlite:db/GUIA_MICHELIN.db";
+		try {
+			is = new FileInputStream("ConfigDB.properties");
+			//Controlamos la exception en caso de que el nombre o la ruta sean incorrecta
+			prop.load(is);
+			
+			driver = prop.getProperty("DRIVER");
+			url = prop.getProperty("URL");
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			// Esta exception la genera si hay un error en la entra o salida con la infomación del fichero 
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public Connection getConnection() throws ClassNotFoundException, SQLException {
